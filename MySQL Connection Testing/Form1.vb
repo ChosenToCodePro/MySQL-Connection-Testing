b6cd70a84a7f813aa50cb3ef2db1ef624f3f2636
ChosenToCodePro <cabralproedition@gmail.com>
2024-11-12 19:56:59 +0800

    Initial Commit

---
+++ MySQL Connection Testing/Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim c As MySqlConnection
+    Dim cmd As MySqlCommand
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        c = New MySqlConnection
+        c.ConnectionString = "server=127.0.0.1;userid=ChosenProSQLX;password='#Ndrdaoi13';database=chinookemc"
+
+        Try
+            c.Open()
+            MessageBox.Show("Wow thats a success UwU", "Testing was Successful")
+            c.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            c.Close()
+        End Try
+    End Sub
+End Class
